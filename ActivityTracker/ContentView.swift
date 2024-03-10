c3b5262df71801d023bf1218b6350e24718eee3d
Tucker <borisblade17@gmail.com>
2024-03-09 23:31:38 -0600

    convert goal views to mvvm - tucker

--- ActivityTracker/ContentView.swift
+++ ActivityTracker/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $selection) {
-            HomeView()
+            GoalsView()
                 .tabItem {
                     Label("Goals", systemImage: "list.clipboard")
                 }
---
+++ ActivityTracker/ViewModels/GoalsView-ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsView-ViewModel.swift
+//  ActivityTracker
+//
+//  Created by tucker bichsel on 09/03/2024.
+//
+
+import Foundation
+
+extension GoalsView {
+    
+    @Observable
+    class ViewModel {
+        var showAddGoal = false
+        var showAddPerson = false
+    }
+}
