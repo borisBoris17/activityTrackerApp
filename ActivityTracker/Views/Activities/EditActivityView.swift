929bad7cb3fc42487f8669fb9be4b73007f32afa
Tucker <borisblade17@gmail.com>
2024-04-04 22:19:07 -0500

    Saving indicator on edit activity and edit person screen - Tucker

--- ActivityTracker/Views/Activities/EditActivityView.swift
+++ ActivityTracker/Views/Activities/EditActivityView.swift
@@ -25,124 +25,142 @@ struct EditActivityView: View {
     
     @State private var showEditGoals = false
     @State private var showDeleteAlert = false
-
+    @State private var isSaving = false
+    
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        NavigationStack {
-            Form {
-                Section("Activity Name") {
-                    TextField("Name", text: $newActivityName)
-                }
-                
-                Section("description") {
-                    TextEditor( text: $newActivityDesc)
-                        .frame(minHeight: 150,
-                               maxHeight: .infinity,
-                               alignment: .center )
-                }
-                
-                Section("Image") {
-                    HStack {
-                        ImagePickerView(photoItem: $newActivityPhotoItem, selectedImageData: $newActivityImageData, imageSize: geometry.size.width * 0.15)
-                            .onChange(of: newActivityImageData) {
-                                if let activityImageData = newActivityImageData,
-                                   let uiImage = UIImage(data: activityImageData) {
-                                    newActivityImage = Image(uiImage: uiImage)
+        ZStack {
+            NavigationStack {
+                Form {
+                    Section("Activity Name") {
+                        TextField("Name", text: $newActivityName)
+                    }
+                    
+                    Section("description") {
+                        TextEditor( text: $newActivityDesc)
+                            .frame(minHeight: 150,
+                                   maxHeight: .infinity,
+                                   alignment: .center )
+                    }
+                    
+                    Section("Image") {
+                        HStack {
+                            ImagePickerView(photoItem: $newActivityPhotoItem, selectedImageData: $newActivityImageData, imageSize: geometry.size.width * 0.15)
+                                .onChange(of: newActivityImageData) {
+                                    if let activityImageData = newActivityImageData,
+                                       let uiImage = UIImage(data: activityImageData) {
+                                        newActivityImage = Image(uiImage: uiImage)
+                                    }
+                                }
+                            
+                            if newActivityPhotoItem == nil {
+                                Spacer()
+                                newActivityImage?
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geometry.size.width * 0.15, height: geometry.size.width * 0.15)
+                            }
+                        }
+                    }
+                    
+                    Section("Goals") {
+                        if showEditGoals {
+                            Button("Close Goals") {
+                                showEditGoals.toggle()
+                            }
+                            GoalSelectionView(selectedGoals: $newActivityGoals)
+                        } else {
+                            Button("Edit Goals") {
+                                showEditGoals.toggle()
+                            }
+                        }
+                    }
+                    
+                    Section("Duration") {
+                        HStack {
+                            Picker("Hours", selection: $newActivityHours) {
+                                ForEach(0..<25) {
+                                    Text("\($0)")
                                 }
                             }
-                        
-                        if newActivityPhotoItem == nil {
-                            Spacer()
-                            newActivityImage?
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geometry.size.width * 0.15, height: geometry.size.width * 0.15)
-                        }
-                    }
-                }
-                
-                Section("Goals") {
-                    if showEditGoals {
-                        Button("Close Goals") {
-                            showEditGoals.toggle()
-                        }
-                        GoalSelectionView(selectedGoals: $newActivityGoals)
-                    } else {
-                        Button("Edit Goals") {
-                            showEditGoals.toggle()
-                        }
-                    }
-                }
-                
-                Section("Duration") {
-                    HStack {
-                        Picker("Hours", selection: $newActivityHours) {
-                            ForEach(0..<25) {
-                                Text("\($0)")
+                            .pickerStyle(.wheel)
+                            .frame(maxHeight: 150)
+                            .clipped()
+                            
+                            VStack {
+                                Spacer()
+                                Text("Hrs")
+                                Spacer()
                             }
-                        }
-                        .pickerStyle(.wheel)
-                        .frame(maxHeight: 150)
-                        .clipped()
-                        
-                        VStack {
-                            Spacer()
-                            Text("Hrs")
-                            Spacer()
-                        }
-                        .frame(maxHeight: 150)
-                        .clipped()
-                        
-                        Picker("Minutes", selection: $newActivityMinutes) {
-                            ForEach(0..<60) {
-                                Text("\($0)")
+                            .frame(maxHeight: 150)
+                            .clipped()
+                            
+                            Picker("Minutes", selection: $newActivityMinutes) {
+                                ForEach(0..<60) {
+                                    Text("\($0)")
+                                }
                             }
+                            .pickerStyle(.wheel)
+                            .frame(maxHeight: 150)
+                            .clipped()
+                            
+                            VStack {
+                                Spacer()
+                                Text("Min")
+                                Spacer()
+                            }
+                            .frame(maxHeight: 150)
+                            .clipped()
                         }
-                        .pickerStyle(.wheel)
-                        .frame(maxHeight: 150)
-                        .clipped()
-                        
-                        VStack {
-                            Spacer()
-                            Text("Min")
-                            Spacer()
+                    }
+                    
+                    Section {
+                        Button("Delete") {
+                            showDeleteAlert = true
                         }
-                        .frame(maxHeight: 150)
-                        .clipped()
+                        .alert(isPresented: $showDeleteAlert) {
+                            Alert(
+                                title: Text("Delete Activity?"),
+                                message: Text("This is a permanent action."),
+                                primaryButton: .destructive(Text("Delete")) {
+                                    deleteActivity(activity)
+                                    dismiss()
+                                },
+                                secondaryButton: .cancel())
+                        }
                     }
                 }
-                
-                Section {
-                    Button("Delete") {
-                        showDeleteAlert = true
+                .navigationTitle("Edit Activity")
+                .toolbar {
+                    ToolbarItem {
+                        Button("Save") {
+                            isSaving = true
+                        }
+                        .padding()
                     }
-                    .alert(isPresented: $showDeleteAlert) {
-                        Alert(
-                            title: Text("Delete Activity?"),
-                            message: Text("This is a permanent action."),
-                            primaryButton: .destructive(Text("Delete")) {
-                                deleteActivity(activity)
-                                dismiss()
-                            },
-                            secondaryButton: .cancel())
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Back") {
+                            dismiss()
+                        }
                     }
                 }
             }
-            .navigationTitle("Edit Activity")
-            .toolbar {
-                ToolbarItem {
-                    Button("Save") {
-                        saveActivity()
-                        dismiss()
-                    }
-                    .padding()
+            
+            if isSaving {
+                ZStack {
+                    Color.black.opacity(0.5)
+                        .ignoresSafeArea()
+                    
+                    ProgressView()
+                        .scaleEffect(5)
                 }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("Back") {
-                        dismiss()
-                    }
-                }
+            }
+        }
+        .onChange(of: isSaving) {
+            Task {
+                dismiss()
+                saveActivity()
             }
         }
     }
--- ActivityTracker/Views/People/EditPersonView.swift
+++ ActivityTracker/Views/People/EditPersonView.swift
@@ -20,73 +20,92 @@ struct EditPersonView: View {
     
     @State private var showAddGoalSheet = false
     @State private var showDeleteAlert = false
+    @State private var isSaving = false
+    
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        NavigationStack {
-            Form {
-                Section("Activity Name") {
-                    TextField("Name", text: $newPersonName)
-                }
-                
-                Section("Image") {
-                    HStack {
-                        ImagePickerView(photoItem: $newPersonPhotoItem, selectedImageData: $newPersonImageData, imageSize: geometry.size.width * 0.15)
-                            .onChange(of: newPersonImageData) {
-                                if let personImageData = newPersonImageData,
-                                   let uiImage = UIImage(data: personImageData) {
-                                    newPersonImage = Image(uiImage: uiImage)
+        ZStack {
+            NavigationStack {
+                Form {
+                    Section("Activity Name") {
+                        TextField("Name", text: $newPersonName)
+                    }
+                    
+                    Section("Image") {
+                        HStack {
+                            ImagePickerView(photoItem: $newPersonPhotoItem, selectedImageData: $newPersonImageData, imageSize: geometry.size.width * 0.15)
+                                .onChange(of: newPersonImageData) {
+                                    if let personImageData = newPersonImageData,
+                                       let uiImage = UIImage(data: personImageData) {
+                                        newPersonImage = Image(uiImage: uiImage)
+                                    }
                                 }
+                            
+                            if newPersonPhotoItem == nil {
+                                Spacer()
+                                newPersonImage?
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geometry.size.width * 0.15, height: geometry.size.width * 0.15)
                             }
-                        
-                        if newPersonPhotoItem == nil {
-                            Spacer()
-                            newPersonImage?
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geometry.size.width * 0.15, height: geometry.size.width * 0.15)
+                        }
+                    }
+                    
+                    Button("Add Goal") {
+                        showAddGoalSheet.toggle()
+                    }
+                    
+                    Section {
+                        Button("Delete") {
+                            showDeleteAlert = true
+                        }
+                        .alert(isPresented: $showDeleteAlert) {
+                            Alert(
+                                title: Text("Delete \(person.wrappedName)?"),
+                                message: Text("This is a permanent action."),
+                                primaryButton: .destructive(Text("Delete")) {
+                                    deletePerson(person)
+                                    dismiss()
+                                },
+                                secondaryButton: .cancel())
                         }
                     }
                 }
-                
-                Button("Add Goal") {
-                    showAddGoalSheet.toggle()
-                }
-                
-                Section {
-                    Button("Delete") {
-                        showDeleteAlert = true
+                .navigationTitle("Edit Person")
+                .toolbar {
+                    ToolbarItem {
+                        Button("Save") {
+                            isSaving = true
+                        }
+                        .padding()
                     }
-                    .alert(isPresented: $showDeleteAlert) {
-                        Alert(
-                            title: Text("Delete \(person.wrappedName)?"),
-                            message: Text("This is a permanent action."),
-                            primaryButton: .destructive(Text("Delete")) {
-                                deletePerson(person)
-                                dismiss()
-                            },
-                            secondaryButton: .cancel())
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Back") {
+                            dismiss()
+                        }
                     }
                 }
             }
-            .navigationTitle("Edit Person")
-            .toolbar {
-                ToolbarItem {
-                    Button("Save") {
-                        savePerson()
-                        dismiss()
-                    }
-                    .padding()
-                }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("Back") {
-                        dismiss()
-                    }
+            .sheet(isPresented: $showAddGoalSheet) {
+                AddGoalToPersonView(person: person)
+            }
+            
+            if isSaving {
+                ZStack {
+                    Color.black.opacity(0.5)
+                        .ignoresSafeArea()
+                    
+                    ProgressView()
+                        .scaleEffect(5)
                 }
             }
         }
-        .sheet(isPresented: $showAddGoalSheet) {
-            AddGoalToPersonView(person: person)
+        .onChange(of: isSaving) {
+            Task {
+                dismiss()
+                savePerson()
+            }
         }
     }
 }
